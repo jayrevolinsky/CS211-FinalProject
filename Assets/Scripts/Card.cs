3bcc6e27af24c839d474667d0f13f4f1aa7cf7bc
Jay Revolinsky <jr408@humboldt.edu>
2020-04-26 16:12:48 -0700

    Added more card functionality, TO-DO adjust parameters for types

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -7,16 +7,23 @@ public class Card : MonoBehaviour
     public int _CardID; //identifier label of card
     public string _CardName; //name of card
     public int _CardMana;  //mana cost of card
-    public string _CardType; //damage type of card
+    public string _CardPrimaryType; //type of card
+    public string _CardSecondaryType; //damage type of card
+
+    public Color MonsterPrimaryType = new  Color(1.0f, 0.0f, 0.0f, 1.0f); //red
+    public Color SpellPrimaryType = new Color(0.0f, 0.0f, 1.0f, 1.0f); //blue
+    public Color TerrainPrimaryType = new Color(1.0f, 0.92f, 0.016f, 1.0f); //yellow
+
     public string _CardDescription; //written description of card effect
 
     //constructor for card variables
-    public Card(int CardID, string CardName, int CardMana, string CardType, string CardDescription)
+    public Card(int CardID, string CardName, int CardMana, string CardPrimaryType, string CardSecondaryType, string CardDescription)
     {
         _CardID = CardID;
         _CardName = CardName;
         _CardMana = CardMana;
-        _CardType = CardType;
+        _CardPrimaryType = CardPrimaryType;
+        _CardSecondaryType = CardSecondaryType;
         _CardDescription = CardDescription;
     }
 }
---
+++ Assets/Scripts/DrawnCard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DrawnCard : MonoBehaviour
+{
+    public List<Card> _DrawnCard = new List<Card>();
+    public int _DrawnID;
+
+    public int CardID;
+    public string CardName;
+    public int CardMana;
+    public string CardPrimaryType;
+    public string CardSecondaryType;
+    public string CardDescription;
+
+    public Text NameText;
+    public Text ManaText;
+    public Text PrimaryTypeColor;
+    public Text SecondaryTypeColor;
+    public Text DescriptionText;
+    
+    void Start()
+    {
+        _DrawnCard[0] = CardDatabase.cardList[_DrawnID];
+    }
+    void Update()
+    {
+        CardID = _DrawnCard[0]._CardID;
+        CardName = _DrawnCard[0]._CardName;
+        CardMana = _DrawnCard[0]._CardMana;
+        CardPrimaryType = _DrawnCard[0]._CardPrimaryType;
+        CardSecondaryType = _DrawnCard[0]._CardSecondaryType;
+        CardDescription = _DrawnCard[0]._CardDescription;
+
+        NameText.text = "" + CardName;
+        ManaText.text = "" + CardMana;
+        PrimaryTypeColor.text = "" + CardPrimaryType;
+        SecondaryTypeColor.text = "" + CardSecondaryType;
+        DescriptionText.text = "" + CardDescription;
+
+    }
+}
