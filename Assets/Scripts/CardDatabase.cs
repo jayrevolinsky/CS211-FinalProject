cef58700d4c5cf7d84259e8241c3e8d6d40c12d5
Jay Revolinsky <jr408@humboldt.edu>
2020-04-30 16:32:21 -0700

    Updated Cards to have full functionality. Big thanks to Brackeys

--- Assets/Scripts/CardDatabase.cs
+++ Assets/Scripts/CardDatabase.cs
@@ -8,27 +8,27 @@ public class CardDatabase : MonoBehaviour
 
     public void Awake() //when game starts build cards
     {
-        cardList.Add(new Card(0, "Orc", 3, "Monster", "Earth", "Noble and hardy")); //add card data
-        cardList.Add(new Card(1, "Dragon", 10, "Monster", "Fire", "Terror of the skies"));
-        cardList.Add(new Card(2, "Kobold", 1, "Monster", "Fire", "The worst pest"));
-        cardList.Add(new Card(3, "Skeleton", 2, "Monster", "Dark", "The remains of an unfortunate being"));
-        cardList.Add(new Card(4, "Ilithid", 4, "Monster", "Dark", "A mind flayer"));
-        cardList.Add(new Card(5, "Card6", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(6, "Card7", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(7, "Card8", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(8, "Card9", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(9, "Card10", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(10, "Card11", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(11, "Card12", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(12, "Card13", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(13, "Card14", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(14, "Card15", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(15, "Card16", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(16, "Card17", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(17, "Card18", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(18, "Card19", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(19, "Card20", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(20, "Card21", 0, "NoPType", "NoSType", "NoDescription"));
-        cardList.Add(new Card(21, "Card22", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(0, "Orc", 3, "Monster", "Earth", "Noble and hardy")); //add card data
+    //    cardList.Add(new Card(1, "Dragon", 10, "Monster", "Fire", "Terror of the skies"));
+    //    cardList.Add(new Card(2, "Kobold", 1, "Monster", "Fire", "The worst pest"));
+    //    cardList.Add(new Card(3, "Skeleton", 2, "Monster", "Dark", "The remains of an unfortunate being"));
+    //    cardList.Add(new Card(4, "Ilithid", 4, "Monster", "Dark", "A mind flayer"));
+    //    cardList.Add(new Card(5, "Card6", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(6, "Card7", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(7, "Card8", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(8, "Card9", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(9, "Card10", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(10, "Card11", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(11, "Card12", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(12, "Card13", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(13, "Card14", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(14, "Card15", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(15, "Card16", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(16, "Card17", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(17, "Card18", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(18, "Card19", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(19, "Card20", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(20, "Card21", 0, "NoPType", "NoSType", "NoDescription"));
+    //    cardList.Add(new Card(21, "Card22", 0, "NoPType", "NoSType", "NoDescription"));
     }
 }
