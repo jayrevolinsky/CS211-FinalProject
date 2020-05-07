9854339a2c521d51d34f34cb37e97e0730d39e60
Jay Revolinsky <jr408@humboldt.edu>
2020-05-07 01:50:40 -0700

    TO-DO fix Monster Invulnerability bug (likely linked to player movement, KeeperMotor.cs or KeeperController.cs)

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region PlayerInstance
+
+    public static PlayerManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    public GameObject player;
+
+    public void KillPlayer ()
+    {
+        //scene restart function
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
